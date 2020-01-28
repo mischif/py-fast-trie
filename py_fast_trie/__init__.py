50817e3fdc3e8c970f00b626d039b3765e70eeda
Jeremy Brown <mischif@users.noreply.github.com>
2020-01-27 19:08:43 -0500

    [skip ci] Added support for reading version from package

--- py_fast_trie/__init__.py
+++ py_fast_trie/__init__.py
@@ -7,5 +7,13 @@
 #       Released under version 3.0 of the Non-Profit Open Source License       #
 ################################################################################
 
+from io import open
+from os.path import abspath, dirname, join
+
 from py_fast_trie.x_fast import XFastTrie
 from py_fast_trie.y_fast import YFastTrie
+
+module_root = dirname(abspath(__file__))
+
+with open(join(module_root, "VERSION"), encoding="utf-8") as version_file:
+	__version__ = version_file.read().strip()
